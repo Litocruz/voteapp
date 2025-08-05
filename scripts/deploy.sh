4ef6a553b1e19540a6f491813650c23d41dd20e1
Litocruz <julian.lamadrid@gmail.com>
2025-08-05 17:18:00 +0200

    Mejor manejo de variable en deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,9 +5,11 @@ ENV=$1
 # Navegar al directorio del proyecto
 cd /home/jlamadrid/code/dia21/roxs-voting-app
 
-# Cargar variables de entorno
+# Cargar variables de entorno de forma robusta
 if [ -f .env ]; then
-  export $(cat .env | sed 's/#.*//g' | xargs)
+  set -a
+  source .env
+  set +a
 fi
 
 # Detener los contenedores actuales
