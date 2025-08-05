83c04a5140fbacb709665fd04cb0a90ddb4cea08
Litocruz <julian.lamadrid@gmail.com>
2025-08-05 17:02:31 +0200

    FIX de variables en deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,11 @@ ENV=$1
 # Navegar al directorio del proyecto
 cd /home/jlamadrid/code/dia21/roxs-voting-app
 
+# Cargar variables de entorno
+if [ -f .env ]; then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
 # Detener los contenedores actuales
 docker compose -f docker-compose.${ENV}.yml down
 
